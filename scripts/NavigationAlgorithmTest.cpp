56aa57270635790c941c6b94382642598c5183a9
irfrenchfry01 <irfrenchfry01@gmail.com>
2017-06-14 18:52:54 -0700

    Added Navigation Algorithm Script
    Also added scripting environment. Need to add common libraries

---
+++ scripts/NavigationAlgorithmTest.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+
+int main()
+{
+	PrintMenu();
+}
+
+void PrintMenu()
+{
+	cout<<"Navigation Algroithm Test";
+	cout<<"+: Add waypoint (Lat: xx.xxxxxx, Long: yy.yyyyyy);
+}
