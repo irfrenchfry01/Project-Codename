370011c7f7a97f02512fce0e8d33e6a1de063963
irfrenchfry01 <irfrenchfry01@gmail.com>
2017-06-14 19:56:06 -0700

    Added Command Input to algorithm

--- scripts/NavigationAlgorithmTest.cpp
+++ scripts/NavigationAlgorithmTest.cpp
@@ -1,13 +1,44 @@
-#include<iostream.h>
-#include<conio.h>
-
-int main()
-{
-	PrintMenu();
-}
+#include<iostream>
 
 void PrintMenu()
 {
-	cout<<"Navigation Algroithm Test";
-	cout<<"+: Add waypoint (Lat: xx.xxxxxx, Long: yy.yyyyyy);
+	std::cout<<"Navigation Algroithm Test\n";
+	std::cout<<"+: Add waypoint (Lat: xx.xxxxxx, Long: yy.yyyyyy)\n";
+	std::cout<<"q: Quit\n";
 }
+
+std::string GetCommand()
+{
+	std::string cmd;
+	std::getline(std::cin, cmd);
+	//Remove the new line character at the end of the string
+	//cmd.erase(cmd.size() - 1);
+	return cmd;
+}
+
+void AddPoint()
+{
+
+}
+
+int main()
+{
+	bool exit = false;
+	std::string cmd = "";
+	while(!exit)
+	{
+		PrintMenu();
+		//Receive the command
+		cmd = GetCommand();
+		if(cmd.compare("q")==0)
+		{
+			std::cout<<"Exiting Script";
+			exit = true;	
+		}
+		else
+		{
+			std::cout<<"Invalid Command\n";
+		}
+	}
+}
+
