3271dd714a3e4a468064f4fcf7f6dfb5d082c41d
Garrett Clay <clayg@onid.orst.edu>
2017-11-01 16:31:14 -0700

    Motors work! Added init and basic motor functionality.

--- arduino_src/Main/motor.cpp
+++ arduino_src/Main/motor.cpp
@@ -5,35 +5,136 @@
  */
 #include "motor.h"
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo MotorFL;   // MOTOR_FL, pin 2
+Servo MotorFR;   // MOTOR_FR, pin 3
+Servo MotorBL;   // MOTOR_BL, pin 4
+Servo MotorBR;   // MOTOR_BR, pin 5
+
+uint8_t FLMotorSpeed[ MOTOR_SPEED_AVG_CNT ] = { 0 };
 
 /*
  *  @brief setup PWM pins for output
  */
 void motor::InitMotorPins(void)
 {
-  pinMode(FL, OUTPUT);
-  pinMode(FR, OUTPUT);
-  pinMode(BL, OUTPUT);
-  pinMode(BR, OUTPUT);
+  uint8_t InitSpeed = 30;
+  uint8_t Speed = 0;
+
+  MotorFL.attach(MOTOR_FL);
+  MotorFR.attach(MOTOR_FR);
+  MotorBL.attach(MOTOR_BL);
+  MotorBR.attach(MOTOR_BR);
+
+  //Ramp speed sequence
+  for(Speed = InitSpeed; Speed < 40; Speed++)
+  {
+    SetMotorSpeed(MOTOR_FL, Speed);
+    SetMotorSpeed(MOTOR_FR, Speed);
+    SetMotorSpeed(MOTOR_BL, Speed);
+    SetMotorSpeed(MOTOR_BR, Speed);
+    delay(1000);
+  }
+
+  //Set speed low, this also sets motors to off
+  SetMotorSpeed(MOTOR_FL, InitSpeed);
+  SetMotorSpeed(MOTOR_FR, InitSpeed);
+  SetMotorSpeed(MOTOR_BL, InitSpeed);
+  SetMotorSpeed(MOTOR_BR, InitSpeed);
+}
+
+/*
+ *  @brief calc motor speed based on averaging scheme
+ *  
+ *  @param[in] Motor      select which motor to calc for
+ *  @param[in] NewSpeed   new speed input to be averaged, in percent
+ *  
+ *  @retval   Speed       current speed to change motor to, in percent
+ */
+uint8_t motor::CalcMotorSpeed(MotorId_t Motor, uint8_t NewSpeed)
+{
+  /* //compile error issue
+  uint8_t i = 0;
+  double Sum = 0;
+  double AvgSpeed = 0;
+  
+  if(MOTOR_FL == Motor)
+  {
+    for(i = 1; i < MOTOR_SPEED_AVG_CNT; i++)
+    {
+      //Sum += (FLMotorSpeed[i] * MOTOR_SPEED_COEF[i-1]);
+      Sum += (FLMotorSpeed[i] * MOTOR_SPEED_COEF);
+      FLMotorSpeed[i-1] = FLMotorSpeed[i];
+    }
+    FLMotorSpeed[MOTOR_SPEED_AVG_CNT] = NewSpeed;
+    Sum += FLMotorSpeed[MOTOR_SPEED_AVG_CNT];
+    AvgSpeed = Sum / MOTOR_SPEED_AVG_CNT;
+  }
+  */
+  
+  /* //commented out for motor test
+  else if(MOTOR_FR == Motor)
+  {
+    
+  }
+  else if(MOTOR_BL == Motor)
+  {
+    
+  }
+  else
+  {
+    
+  }
+  */
+  return 0;
 }
 
 /*
  *  @brief set PWM duty cycle for motor speed control
  *  
  *  note: motor speed input is in percent but get scaled between
- *  0 and 255
+ *  60 and 180
  *  
  *  param[in] Motor         type MotorId_t for which motor affect
- *  param[in] SpeedPercent  0-100% of motor speed
+ *  param[in] SpeedPercent  34-100% of motor speed
  */
-void motor::SetMotorSpeed(MotorId_t Motor, uint8_t SpeedPercent)
+MotorStatus_t motor::SetMotorSpeed(MotorId_t Motor, uint8_t SpeedPercent)
 {
+  MotorStatus_t Status = MOTOR_STATUS_ERROR;
   uint8_t Speed = 0;
   if(SpeedPercent <= 100)
   {
-    Speed = SpeedPercent * 2.55;
-    analogWrite(Motor, Speed); 
+    Speed = SpeedPercent * 1.8;
+    Serial.print("\nSpeed % = "); Serial.print(SpeedPercent); Serial.print(" Speed = "); Serial.print(Speed);
+     
+    if(MOTOR_FL == Motor)
+    {
+      MotorFL.write(Speed);
+    }
+    else if(MOTOR_FR == Motor)
+    {
+      MotorFR.write(Speed);
+    }
+    else if(MOTOR_BL == Motor)
+    {
+      MotorBL.write(Speed);
+    }
+    else if(MOTOR_BR == Motor)
+    {
+      MotorBR.write(Speed);
+    }
+    else
+    {
+      Status = MOTOR_STATUS_INVALID_MOTOR_ID;
+    }
   }
+  else
+  {
+    Status = MOTOR_STATUS_ERROR;
+  }
+
+  return Status;
 }
 
 /*
@@ -44,11 +145,11 @@ void motor::SetMotorSpeed(MotorId_t Motor, uint8_t SpeedPercent)
  */
 void motor::EmergencyMotorOff(void)
 {
-  SetMotorSpeed(FL, MOTOR_OFF);
-  SetMotorSpeed(FR, MOTOR_OFF);
-  SetMotorSpeed(BL, MOTOR_OFF);
-  SetMotorSpeed(BR, MOTOR_OFF);
-
+  SetMotorSpeed(MOTOR_FL, MOTOR_OFF);
+  SetMotorSpeed(MOTOR_FR, MOTOR_OFF);
+  SetMotorSpeed(MOTOR_BL, MOTOR_OFF);
+  SetMotorSpeed(MOTOR_BR, MOTOR_OFF);
+  
   while(true);
 }
 
--- arduino_src/Main/motor.h
+++ arduino_src/Main/motor.h
@@ -11,26 +11,42 @@
 
 #define MOTOR_OFF 0u
 
+// use motor speed coefficient to weight newer vs older motor speeds
+#define MOTOR_SPEED_AVG_CNT 3u     //note, add coefs if adding more to average
+//#define MOTOR_SPEED_COEF[MOTOR_SPEED_AVG_CNT] = { 0.8 1.0 1.2 } // oldest speed to newest
+#define MOTOR_SPEED_COEF 1 // oldest speed to newest
+
 typedef enum _MotorId_t{
-  FL = 2,   //front left motor
-  FR = 3,   //front right motor
-  BL = 4,   //back left motor
-  BR = 5   //back right motor
+  MOTOR_FL = 2,   //front left motor
+  MOTOR_FR = 3,   //front right motor
+  MOTOR_BL = 4,   //back left motor
+  MOTOR_BR = 5   //back right motor
 } MotorId_t;
 
+typedef enum _MotorStatus_t{
+  MOTOR_STATUS_SUCCESS            = 0,   //successful motor function execution
+  MOTOR_STATUS_ERROR              = 1,
+  MOTOR_STATUS_INVALID_MOTOR_ID   = 2    //invalid motor identifier
+} MotorStatus_t;
 
 class motor {
   public:
     //setup PWM pins for motors
     void InitMotorPins(void);
+
+    uint8_t CalcMotorSpeed(MotorId_t Motor, uint8_t NewSpeed);
     
     //set motor speed
-    void SetMotorSpeed(MotorId_t Motor, uint8_t SpeedPercent);
+    MotorStatus_t SetMotorSpeed(MotorId_t Motor, uint8_t SpeedPercent);
 
     //turns all motors off and hangs program
     void EmergencyMotorOff(void);
   private:
 
+    //variable to track set motor speeds
+    //note, these are not actual values as those will be calculated based on this array
+
+    
 };
 
- #endif
+#endif
